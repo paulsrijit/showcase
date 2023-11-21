5be6c7d11ecd567381abb1aa80631a2b237a7d86
Srijit Paul <paul.srijit@gmail.com>
2023-11-22 01:09:05 +0530

    Initial Commit
    Created a simple kafka consumer within spring-boot app

---
+++ broker-agnostic-enabler/src/main/java/org/paulsr/showcase/brokag/config/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,52 @@
+package org.paulsr.showcase.brokag.config.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+	@Value("${brokag.kafka.bootstrap.servers}")
+	private String bootstrapAddress;
+
+	@Value("${brokag.kafka.groupId}")
+	private String groupId;
+
+//	@Value("${brokag.kafka.topicName}")
+//	private String topicName;
+
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		return new DefaultKafkaConsumerFactory<>(props);
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+
+		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		return factory;
+	}
+
+	@KafkaListener(topics = "PAULSR_DEMO_T001", groupId = "kafka-cons-grp-001")
+	public void listenGroupFoo(String message) {
+		System.out.println("Received Message in group foo: " + message);
+	}
+}
